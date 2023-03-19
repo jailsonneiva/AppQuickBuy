3c157cfed6b1b16da42c6cdd925e83153a12fa7e
Jailson Neiva <jailsonneiva@hotmail.com>
2023-03-19 20:23:23 -0300

    modificações-2

--- modeloBlog/tela-inicial/tela.js
+++ modeloBlog/tela-inicial/tela.js
@@ -1,65 +1,53 @@
-/*
-function list(){ 
-  var texto= document.getElementById("texto").value;
-  var inputSome =document.querySelector("#texto").style.zIndex=0;
-
-    if(texto && texto=="tesoura"){ 
-      document.querySelector("#p1").style.display="block"; 
-    }else if(texto && texto!="tesoura"){
-      document.getElementById("p2").innerHTML="Inválido";
-      inputSome
-    }else{
-      document.getElementById("p2").innerHTML="Digite alguma coisa";
-    }
-}
-document.getElementById("btn1").addEventListener("click", list);
-*/
 
 
-// FUNÇÃO LIST CLICK...........
 
 document.getElementById("texto").focus();
 var p = document.createElement("h1");
 var txtNode = document.createTextNode("digite - \"tesoura\" ");
 p.appendChild(txtNode);
 
-
+//...........FUNÇÃO REMOVE...................
+function remove(){ 
+  document.getElementById("botoes").appendChild(p).remove();
+  document.querySelector("#p2").innerHTML="";
+  document.querySelector("#texto").style.zIndex=1;
+  document.getElementById("texto").focus();
+}
+//...........FUNÇÃO LIST...................
 function list(){ 
- 
-  
+
   var texto= document.getElementById("texto").value.toLowerCase();
   var zIndex = document.getElementById("texto").style.zIndex=0;
 
  if(texto && texto=="tesoura"){ 
       document.querySelector("#p1").style.display="block";
       document.getElementById("texto").style.zIndex=1; 
-  
+      
     }else if(texto && texto!="tesoura"){
       document.getElementById("p2").innerHTML="Inválido";
-      zIndex;            
-      document.getElementById("texto").focus();
-        function remove(){
-          document.getElementById("botoes").appendChild(p);
-        }
-        setTimeout(remove, 2000).remove()         
+      zIndex;
+      document.getElementById("texto").value=""; 
+      document.getElementById("botoes").appendChild(p);
+        setTimeout(remove, 3500).remove() 
 
     }else {
     document.getElementById("p2").innerHTML="Digite alguma coisa";
       zIndex;
-      document.getElementById("texto").value="";    
+      document.getElementById("texto").value="";  
+      setTimeout(remove, 3000).remove()   
     }
 } 
 document.getElementById("btn1").addEventListener("click", list);
 
-// FUNÇÃO LIMPAR .........
+// .....FUNÇÃO LIMPAR .....................
 
 function limpar(){    
   document.querySelector("#p2").innerHTML="";
   document.querySelector("#texto").value="";
   document.querySelector("#p1").style.display="none"; 
   document.querySelector("#texto").style.zIndex=1;
-  document.getElementById("texto").focus();
   document.getElementById("botoes").appendChild(p).remove();
+  document.getElementById("texto").focus();
 }
 document.getElementById("btn2").addEventListener("click", limpar);
 
