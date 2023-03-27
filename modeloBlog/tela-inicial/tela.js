1057c2c44658518204a72769b9559e1008fb55f4
Jailson Neiva <86751553+jailsonneiva@users.noreply.github.com>
2023-03-27 01:06:50 -0300

    Update tela.js
    Anulado função Enter para funcionar...

--- modeloBlog/tela-inicial/tela.js
+++ modeloBlog/tela-inicial/tela.js
@@ -1,9 +1,13 @@
 
+
 document.getElementById("texto").focus();
+
 var p = document.createElement("h2");
 var txtNode = document.createTextNode("digite - \' tesoura \' ");
 p.appendChild(txtNode);
+
 function estilo(){
+  p.style.backgroundColor="blue"
   p.style.backgroundColor="blue";
   p.style.padding="7px 20px";  
   p.style.margin="20px auto 0";
@@ -15,19 +19,20 @@ function estilo(){
 //...........FUNÇÃO REMOVE...................
 function remover(){ 
   document.getElementById("botoes").appendChild(p).remove();
+  document.querySelector("#p2").innerHTML="";
   document.getElementById("p2").innerHTML="";
   document.querySelector("#texto").style.zIndex=1;
   document.getElementById("texto").focus();
   document.querySelector("#p1").style.display="none";
   document.querySelector("Option").innerHTML="";
   document.querySelector("#texto").value="";
-
 }
 
 //...........FUNÇÃO LOJAS...................
 
 function lojas(){ 
-   var texto= document.getElementById("texto").value.toLowerCase();
+  var texto = document.getElementById("texto").value.toLowerCase();
+  
   if (texto && texto=="tesoura") { 
      document.querySelector("#p1").style.display="block";
      document.getElementById("form-input").reset();
@@ -35,17 +40,20 @@ function lojas(){
      document.getElementById("texto").focus();
      document.getElementById("p2").style.display="none";
      document.getElementById("texto").style.zIndex=1;
-     
+
   } else if (texto && texto!="tesoura") {
      document.getElementById("p2").innerHTML="Inválido";
      document.getElementById("texto").style.zIndex=0;
      document.querySelector("#p1").style.display="none";
+     document.getElementById("form-input").reset(); 
      document.getElementById("botoes").appendChild(p);
+     estilo()
+     setTimeout(remover, 2000).remove() 
      estilo();
      document.querySelector("#texto").value="";
      setTimeout(remover, 2000).remove(); 
 
-  } else {
+  } else {     
      document.getElementById("p2").innerHTML="Digite alguma coisa"; 
      document.getElementById("texto").style.zIndex=0;
     setTimeout(remover, 1500).remove();   
@@ -53,13 +61,15 @@ function lojas(){
 } 
 document.getElementById("btn1").addEventListener("click", lojas);
 
+/*
 //.........FUNÇÃO ENTER..................
 lojas().addEventListener("keypress", function(e) {
   if (e.key === "Enter") {
     e.preventDefault();
     document.getElementById("btn1").click();
   } 
-}); 
+});
+*/
 
 // .....FUNÇÃO LIMPAR .....................
 
@@ -67,25 +77,9 @@ function limpar(){
   document.querySelector("#p2").innerHTML="";
   document.getElementById("form-input").reset();
   document.querySelector("#p1").style.display="none"; 
-  let clearText = document.querySelector("#texto");clearText.style.zIndex=1;
-  clearText.style.value="";
+  document.querySelector("#texto").value="";
   document.getElementById("botoes").appendChild(p).remove();
   document.getElementById("texto").focus();
 }
 document.getElementById("btn2").addEventListener("click", limpar);
-
-
-
-/*
-const urlUF = "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
-
-window.addEventListener("load", async()=>{
-  const request = await fetch(urlUF);
-  const response = await request.json();
-console.log(response[0].sigla);
-
-
-})
-
-*/
 
